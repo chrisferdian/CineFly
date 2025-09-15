03366d7942478da58ea6a025088540171b9eba30
Chris Ferdian <chris.ferdian@gmail.com>
2025-09-16 00:57:20 +0700

    initial project

---
+++ CineFly/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CineFly
+//
+//  Created by chris on 16/09/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
